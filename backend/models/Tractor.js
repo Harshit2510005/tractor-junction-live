afceb13e7eb7f2b3f92c2896cd05150bc5ea407e
Kachhadiya Harshit <harshit.kachhadiya1012@ngivbt.edu.in>
2026-01-02 22:47:17 +0530

    Update Tractor.js

--- backend/models/Tractor.js
+++ backend/models/Tractor.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const tractorSchema = new mongoose.Schema({
   brand: { type: String, required: true },
   logo: { type: String, required: true },
-  website: { type: String }, // Official website ke liye
-  model: { type: String },   // Ab ye required nahi hai
-  hp: { type: String },      // Ab ye required nahi hai
-  price: { type: String },   // Ab ye required nahi hai
+  website: { type: String }, 
+  model: { type: String },   
+  hp: { type: String },      
+  price: { type: String },   
   image: { type: String, default: "🚜" }
 });
 
